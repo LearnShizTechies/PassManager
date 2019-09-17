4a861b00942fa4e1aa08a5be02cc72a65c319466
learneradarsh <learner.adarsh@gmail.com>
2019-09-17 18:12:51 +0530

    added db

---
+++ db_con.js
@@ -0,0 +1,12 @@
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
